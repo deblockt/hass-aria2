c30b880dce7162eb15cec888c54a4f8d0f2608d3
Thomas Deblock <deblock.thomas.62@gmail.com>
2022-07-09 17:55:38 +0200

    feat: update refresh duration when no active download
    Use DataUpdateCoordinator to refresh list only when needed.
    Refresh each 3 seconds if a download is active else each 30 seconds

--- custom_components/aria2/__init__.py
+++ custom_components/aria2/__init__.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import timedelta
 import logging
 from typing import List
 
@@ -7,6 +8,9 @@ from custom_components.aria2.aria2_commands import AddUri, DownoladKeys, MultiCa
 
 from .const import DOMAIN, CONF_PORT
 from homeassistant.const import CONF_HOST, CONF_ACCESS_TOKEN
+from homeassistant.helpers.update_coordinator import (
+    DataUpdateCoordinator,
+)
 import aria2p
 import async_timeout
 
@@ -54,19 +58,6 @@ async def async_setup_entry(hass, entry):
         hass.config_entries.async_forward_entry_setup(entry, 'sensor')
     )
 
-    async def refresh_downloads() -> List[aria2p.Download]:
-        async with async_timeout.timeout(10):
-            [active, waiting, stopped] = await ws_client.call(MultiCall([
-                TellActive(keys = DOWNLOAD_DUMP_KEYS),
-                TellWaiting(keys = DOWNLOAD_DUMP_KEYS),
-                TellStopped(keys = DOWNLOAD_DUMP_KEYS)
-            ]))
-
-            downloads = [*active, *waiting, *stopped]
-            _LOGGER.info('download refreshed ' + str(downloads))
-            hass.bus.fire('download_list_updated', {'list': [dump(d) for d in downloads]})
-            return downloads
-
     async def handle_add_download(call):
         """Handle the service call."""
         url = call.data.get('url')
@@ -76,7 +67,7 @@ async def async_setup_entry(hass, entry):
         """Handle the service call."""
         gid = call.data.get('gid')
         await ws_client.call(Remove(gid))
-        await refresh_downloads()
+        await download_list_coordinator.async_refresh()
 
     async def handle_pause_download(call):
         """Handle the service call."""
@@ -88,43 +79,53 @@ async def async_setup_entry(hass, entry):
         gid = call.data.get('gid')
         await ws_client.call(Unpause(gid))
 
-    async def on_download_state_updated(gid, status):
-        hass.bus.fire('download_state_updated', {'gid': gid, 'status': status})
-        await refresh_downloads()
-
-    async def refresh_downloads_loop():
-        while True:
-            downloads: List[aria2p.Download] = []
-            try:
-                downloads = await refresh_downloads()
-            except asyncio.TimeoutError:
-                _LOGGER.warn('fail to refresh downloads due to timeout.')
-                await asyncio.sleep(3)
-                continue
-
-            active_downloads = [d for d in downloads if d.status == 'active']
-            if len(active_downloads) > 0:
-                _LOGGER.debug("wait 3 second before next download list refresh")
-                await asyncio.sleep(3)
-            else:
-                _LOGGER.debug("wait 30 second before next download list refresh")
-                await asyncio.sleep(30)
-
-    async def refresh_downloads_service(call):
-        await refresh_downloads()
-
-    ws_client.on_download_state_updated(on_download_state_updated)
+    async def handler_refresh_downloads(call):
+        await download_list_coordinator.async_refresh()
 
     hass.services.async_register(DOMAIN, 'start_download', handle_add_download)
     hass.services.async_register(DOMAIN, 'remove_download', handle_remove_download)
     hass.services.async_register(DOMAIN, 'pause_download', handle_pause_download)
     hass.services.async_register(DOMAIN, 'resume_download', handle_resume_download)
-    hass.services.async_register(DOMAIN, 'refresh_downloads', refresh_downloads_service)
+    hass.services.async_register(DOMAIN, 'refresh_downloads', handler_refresh_downloads)
 
     hass.loop.create_task(ws_client.listen_notifications())
-    hass.loop.create_task(refresh_downloads_loop())
+
+    async def on_download_state_updated(gid, status):
+        hass.bus.fire('download_state_updated', {'gid': gid, 'status': status})
+        await download_list_coordinator.async_refresh()
+    ws_client.on_download_state_updated(on_download_state_updated)
+
+    download_list_coordinator = init_download_list_update_coordinator()
+    download_list_coordinator.async_add_listener(lambda: hass.bus.fire('download_list_updated', {'list': [dump(d) for d in download_list_coordinator.data]}))
 
     return True
 
+def init_download_list_update_coordinator(hass, ws_client):
+    async def get_downloads() -> List[aria2p.Download]:
+        async with async_timeout.timeout(10):
+            [active, waiting, stopped] = await ws_client.call(MultiCall([
+                TellActive(keys = DOWNLOAD_DUMP_KEYS),
+                TellWaiting(keys = DOWNLOAD_DUMP_KEYS),
+                TellStopped(keys = DOWNLOAD_DUMP_KEYS)
+            ]))
 
+            downloads = [*active, *waiting, *stopped]
 
+            active_downloads = [d for d in downloads if d.status == 'active']
+            if len(active_downloads) > 0:
+                _LOGGER.debug("update the coordinator to refresh each 3 seconds")
+                coordinator.update_interval = timedelta(seconds=3)
+            else:
+                _LOGGER.debug("update the coordinator to refresh each 30 seconds")
+                coordinator.update_interval = timedelta(seconds=30)
+
+            return downloads
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="download_list",
+        update_method=get_downloads,
+        update_interval=timedelta(seconds=3),
+    )
+    return coordinator
