8be5689915e20823f7ea7ab756ff8d5750029027
Thomas Deblock <deblock.thomas.62@gmail.com>
2022-07-12 09:02:36 +0200

    refactor: configuration process
    - remove usage of aria2p for the configuration process

--- custom_components/aria2/config_flow.py
+++ custom_components/aria2/config_flow.py
@@ -1,25 +1,23 @@
 import logging
 import requests
 import json
+import asyncio
+from custom_components.aria2.aria2_client import WSClient
+from custom_components.aria2.aria2_commands import GetGlobalOption, UnauthorizedError
 
 from homeassistant import config_entries
-from .const import DOMAIN, CONF_PORT
+from .const import DOMAIN, CONF_PORT, ws_url
 
 import voluptuous as vol
 
 from homeassistant.core import callback
 from homeassistant.const import CONF_HOST, CONF_ACCESS_TOKEN
-
+import socket
+import websockets
 import aria2p
 import traceback
 
 _LOGGER = logging.getLogger(__name__)
-
-def get_port(host):
-    if host.startswith('https'):
-        return 443
-    else:
-        return 80
 
 class Aria2ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -31,43 +29,30 @@ class Aria2ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         secret = None
         if user_input is not None:
             host = user_input[CONF_HOST]
-            port = user_input[CONF_PORT] if CONF_PORT in user_input else get_port(user_input[CONF_HOST])
+            port = user_input[CONF_PORT] if CONF_PORT in user_input else 6800
             secret = user_input[CONF_ACCESS_TOKEN] if CONF_ACCESS_TOKEN in user_input else None
 
-            aria2 = aria2p.API(
-                aria2p.Client(
-                    host = host,
-                    port = port,
-                    secret = secret
-                )
-            )
+            ws_client = WSClient(ws_url = ws_url(host, port), secret = secret, loop = self.hass.loop, retry_on_connection_error = False)
 
             try:
-                await self.hass.async_add_executor_job(lambda: aria2.get_global_options())
+                await ws_client.call(GetGlobalOption())
 
                 return self.async_create_entry(
                     title="aria 2 configuration",
                     data= {
-                        CONF_HOST:  host,
+                        CONF_HOST: host,
                         CONF_PORT: port,
                         CONF_ACCESS_TOKEN: secret
                     }
                 )
-            except requests.exceptions.InvalidURL:
+            except (socket.gaierror, asyncio.exceptions.TimeoutError, ConnectionRefusedError):
                 _LOGGER.exception("connexion error")
                 errors['base'] = 'invalid_url'
-            except requests.exceptions.ConnectionError:
-                _LOGGER.exception("connexion error")
+            except websockets.exceptions.InvalidStatusCode:
+                _LOGGER.exception("server exists but is not aria2 server")
                 errors['base'] = 'connexion'
-            except json.decoder.JSONDecodeError:
-                _LOGGER.exception("json error")
-                errors['base'] = 'json'
-            except aria2p.client.ClientException as e:
-                _LOGGER.exception("aria2 client exception")
-                if str(e) == 'Unauthorized':
-                    errors['base'] = 'aria_unauthorized'
-                else:
-                    errors['base'] = 'aria_unknown'
+            except UnauthorizedError:
+                errors['base'] = 'aria_unauthorized'
             except:
                 _LOGGER.exception("unknow error")
                 errors['base'] = 'unknown'
--- custom_components/aria2/const.py
+++ custom_components/aria2/const.py
@@ -1,3 +1,16 @@
+import logging
+
+
 DOMAIN="aria2"
 
 CONF_PORT="port"
+
+_LOGGER = logging.getLogger(__name__)
+def ws_url(host: str, port: str):
+    if host.startswith('https'):
+        _LOGGER.warn("https is not supported yet")
+        return f"ws://{host[8:]}:{port}/jsonrpc"
+    elif host.startswith('http'):
+        return f"ws://{host[7:]}:{port}/jsonrpc"
+    else:
+        return f"ws://{host}:{port}/jsonrpc"
