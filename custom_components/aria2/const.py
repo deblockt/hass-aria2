c0712a405f1008e8440c15087d8b7f79dcc8aa58
Thomas Deblock <deblock.thomas.62@gmail.com>
2022-07-14 18:46:11 +0200

    feat: add secure connexion support

--- custom_components/aria2/const.py
+++ custom_components/aria2/const.py
@@ -4,13 +4,14 @@ import logging
 DOMAIN="aria2"
 
 CONF_PORT="port"
+CONF_SERCURE_CONNECTION = 'secure_connection'
 
-_LOGGER = logging.getLogger(__name__)
-def ws_url(host: str, port: str):
+def ws_url(host: str, port: str, secure_socket: bool = False):
     if host.startswith('https'):
-        _LOGGER.warn("https is not supported yet")
-        return f"ws://{host[8:]}:{port}/jsonrpc"
+        return f"wss://{host[8:]}:{port}/jsonrpc"
     elif host.startswith('http'):
         return f"ws://{host[7:]}:{port}/jsonrpc"
+    elif secure_socket:
+        return f"wss://{host}:{port}/jsonrpc"
     else:
         return f"ws://{host}:{port}/jsonrpc"
