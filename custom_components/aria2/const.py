4ebfb1d1349442bf10b8cf3befbc2eaf5975950a
Thomas Deblock <deblock.thomas.62@gmail.com>
2022-07-14 18:17:07 +0200

    feat: add sercure socket support

--- custom_components/aria2/const.py
+++ custom_components/aria2/const.py
@@ -5,11 +5,9 @@ DOMAIN="aria2"
 
 CONF_PORT="port"
 
-_LOGGER = logging.getLogger(__name__)
 def ws_url(host: str, port: str):
     if host.startswith('https'):
-        _LOGGER.warn("https is not supported yet")
-        return f"ws://{host[8:]}:{port}/jsonrpc"
+        return f"wss://{host[8:]}:{port}/jsonrpc"
     elif host.startswith('http'):
         return f"ws://{host[7:]}:{port}/jsonrpc"
     else:
