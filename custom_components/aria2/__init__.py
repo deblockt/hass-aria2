b6a9153fd9fc57830a29e087e15aa7f435f981ea
Thomas Deblock <deblock.thomas.62@gmail.com>
2025-10-13 21:29:49 +0200

    feat: add to add torrent file in add download endpoint

--- custom_components/aria2/__init__.py
+++ custom_components/aria2/__init__.py
@@ -3,7 +3,19 @@ import logging
 from typing import List
 
 from custom_components.aria2.aria2_client import WSClient
-from custom_components.aria2.aria2_commands import AddUri, DownoladKeys, MultiCall, Pause, Remove, TellActive, TellStatus, TellStopped, TellWaiting, Unpause
+from custom_components.aria2.aria2_commands import (
+    AddUri,
+    AddTorrent,
+    DownoladKeys,
+    MultiCall,
+    Pause,
+    Remove,
+    TellActive,
+    TellStatus,
+    TellStopped,
+    TellWaiting,
+    Unpause,
+)
 
 from .const import CONF_SERCURE_CONNECTION, DOMAIN, CONF_PORT, ws_url
 from homeassistant.const import CONF_HOST, CONF_ACCESS_TOKEN, Platform
@@ -28,18 +40,21 @@ DOWNLOAD_DUMP_KEYS = [
     DownoladKeys.BITTORENT,
     DownoladKeys.SEEDER,
     DownoladKeys.UPLOAD_SPEED,
-    DownoladKeys.UPLOADED_LENGTH
+    DownoladKeys.UPLOADED_LENGTH,
 ]
+
+
 def dump_files(files: list[aria2p.File]):
     return [
         {
             "path": f.path,
             "completed_length": f.completed_length,
             "index": f.index,
-            "length": f.length
+            "length": f.length,
         }
         for f in files
     ]
+
 
 def dump(download: aria2p.Download):
     data = {
@@ -50,32 +65,43 @@ def dump(download: aria2p.Download):
         "completed_length": download.completed_length,
         "download_speed": download.download_speed,
         "files": dump_files(download.files),
-        "is_torrent": download.is_torrent
+        "is_torrent": download.is_torrent,
     }
 
     if download.is_torrent:
-        data.update({
-            "seeder": download.seeder,
-            "upload_length": download.upload_length,
-            "upload_speed": download.upload_speed
-        })
+        data.update(
+            {
+                "seeder": download.seeder,
+                "upload_length": download.upload_length,
+                "upload_speed": download.upload_speed,
+            }
+        )
 
     return data
 
+
 async def async_setup_entry(hass, entry):
-    """ a aria sensor """
+    """a aria sensor"""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = dict(entry.data)
 
-    server_url = ws_url(entry.data[CONF_HOST], entry.data[CONF_PORT], entry.data[CONF_SERCURE_CONNECTION] if CONF_SERCURE_CONNECTION in entry.data else False)
-    ws_client = WSClient(ws_url = server_url, secret = entry.data[CONF_ACCESS_TOKEN], loop = hass.loop)
+    server_url = ws_url(
+        entry.data[CONF_HOST],
+        entry.data[CONF_PORT],
+        entry.data[CONF_SERCURE_CONNECTION]
+        if CONF_SERCURE_CONNECTION in entry.data
+        else False,
+    )
+    ws_client = WSClient(
+        ws_url=server_url, secret=entry.data[CONF_ACCESS_TOKEN], loop=hass.loop
+    )
 
-    hass.data[DOMAIN][entry.entry_id]['ws_client'] = ws_client
-    hass.data[DOMAIN][entry.entry_id]['service_attributes'] = {
-        'identifiers': {(DOMAIN, server_url)},
-        'manufacturer': "Aria2",
-        'name': "Aria2",
-        'entry_type': DeviceEntryType.SERVICE
+    hass.data[DOMAIN][entry.entry_id]["ws_client"] = ws_client
+    hass.data[DOMAIN][entry.entry_id]["service_attributes"] = {
+        "identifiers": {(DOMAIN, server_url)},
+        "manufacturer": "Aria2",
+        "name": "Aria2",
+        "entry_type": DeviceEntryType.SERVICE,
     }
 
     hass.async_create_task(
@@ -83,8 +109,16 @@ async def async_setup_entry(hass, entry):
     )
 
     download_list_coordinator = init_download_list_update_coordinator(hass, ws_client)
-    download_list_coordinator.async_add_listener(lambda: hass.bus.fire('download_list_updated', {'server_entry_id': entry.entry_id, 'list': [dump(d) for d in download_list_coordinator.data]}))
-    hass.data[DOMAIN][entry.entry_id]['coordinator'] = download_list_coordinator
+    download_list_coordinator.async_add_listener(
+        lambda: hass.bus.fire(
+            "download_list_updated",
+            {
+                "server_entry_id": entry.entry_id,
+                "list": [dump(d) for d in download_list_coordinator.data],
+            },
+        )
+    )
+    hass.data[DOMAIN][entry.entry_id]["coordinator"] = download_list_coordinator
 
     register_services(hass)
 
@@ -94,61 +128,77 @@ async def async_setup_entry(hass, entry):
         await download_list_coordinator.async_refresh()
         download = await ws_client.call(TellStatus(gid, DOWNLOAD_DUMP_KEYS))
 
-        hass.bus.fire('download_state_updated', {'server_entry_id': entry.entry_id, 'gid': gid, 'status': status, 'download': dump(download)})
+        hass.bus.fire(
+            "download_state_updated",
+            {
+                "server_entry_id": entry.entry_id,
+                "gid": gid,
+                "status": status,
+                "download": dump(download),
+            },
+        )
 
     ws_client.on_download_state_updated(on_download_state_updated)
 
     return True
 
+
 def register_services(hass):
     async def handle_add_download(call):
         """Handle the service call."""
-        url = call.data.get('url')
-        entry_id = call.data.get('server_entry_id')
-        await hass.data[DOMAIN][entry_id]['ws_client'].call(AddUri([url]))
+        url = call.data.get("url")
+        entry_id = call.data.get("server_entry_id")
+        if url.startswith("http"):
+            await hass.data[DOMAIN][entry_id]["ws_client"].call(AddUri([url]))
+        else:
+            await hass.data[DOMAIN][entry_id]["ws_client"].call(AddTorrent(url))
 
     async def handle_remove_download(call):
         """Handle the service call."""
-        gid = call.data.get('gid')
-        entry_id = call.data.get('server_entry_id')
-        await hass.data[DOMAIN][entry_id]['ws_client'].call(Remove(gid))
-        await hass.data[DOMAIN][entry_id]['coordinator'].async_refresh()
+        gid = call.data.get("gid")
+        entry_id = call.data.get("server_entry_id")
+        await hass.data[DOMAIN][entry_id]["ws_client"].call(Remove(gid))
+        await hass.data[DOMAIN][entry_id]["coordinator"].async_refresh()
 
     async def handle_pause_download(call):
         """Handle the service call."""
-        gid = call.data.get('gid')
-        entry_id = call.data.get('server_entry_id')
-        await hass.data[DOMAIN][entry_id]['ws_client'].call(Pause(gid))
+        gid = call.data.get("gid")
+        entry_id = call.data.get("server_entry_id")
+        await hass.data[DOMAIN][entry_id]["ws_client"].call(Pause(gid))
 
     async def handle_resume_download(call):
         """Handle the service call."""
-        gid = call.data.get('gid')
-        entry_id = call.data.get('server_entry_id')
-        await hass.data[DOMAIN][entry_id]['ws_client'].call(Unpause(gid))
+        gid = call.data.get("gid")
+        entry_id = call.data.get("server_entry_id")
+        await hass.data[DOMAIN][entry_id]["ws_client"].call(Unpause(gid))
 
     async def handler_refresh_downloads(call):
-        entry_id = call.data.get('server_entry_id')
-        await hass.data[DOMAIN][entry_id]['coordinator'].async_refresh()
+        entry_id = call.data.get("server_entry_id")
+        await hass.data[DOMAIN][entry_id]["coordinator"].async_refresh()
 
-    hass.services.async_register(DOMAIN, 'start_download', handle_add_download)
-    hass.services.async_register(DOMAIN, 'remove_download', handle_remove_download)
-    hass.services.async_register(DOMAIN, 'pause_download', handle_pause_download)
-    hass.services.async_register(DOMAIN, 'resume_download', handle_resume_download)
-    hass.services.async_register(DOMAIN, 'refresh_downloads', handler_refresh_downloads)
+    hass.services.async_register(DOMAIN, "start_download", handle_add_download)
+    hass.services.async_register(DOMAIN, "remove_download", handle_remove_download)
+    hass.services.async_register(DOMAIN, "pause_download", handle_pause_download)
+    hass.services.async_register(DOMAIN, "resume_download", handle_resume_download)
+    hass.services.async_register(DOMAIN, "refresh_downloads", handler_refresh_downloads)
 
 
 def init_download_list_update_coordinator(hass, ws_client):
     async def get_downloads() -> List[aria2p.Download]:
         async with async_timeout.timeout(10):
-            [active, waiting, stopped] = await ws_client.call(MultiCall([
-                TellActive(keys = DOWNLOAD_DUMP_KEYS),
-                TellWaiting(keys = DOWNLOAD_DUMP_KEYS),
-                TellStopped(keys = DOWNLOAD_DUMP_KEYS)
-            ]))
+            [active, waiting, stopped] = await ws_client.call(
+                MultiCall(
+                    [
+                        TellActive(keys=DOWNLOAD_DUMP_KEYS),
+                        TellWaiting(keys=DOWNLOAD_DUMP_KEYS),
+                        TellStopped(keys=DOWNLOAD_DUMP_KEYS),
+                    ]
+                )
+            )
 
             downloads = [*active, *waiting, *stopped]
 
-            active_downloads = [d for d in downloads if d.status == 'active']
+            active_downloads = [d for d in downloads if d.status == "active"]
             if len(active_downloads) > 0:
                 _LOGGER.debug("update the coordinator to refresh each 3 seconds")
                 coordinator.update_interval = timedelta(seconds=3)
--- custom_components/aria2/aria2_commands.py
+++ custom_components/aria2/aria2_commands.py
@@ -1,4 +1,3 @@
-
 import asyncio
 from asyncio import futures
 from enum import Enum
@@ -12,51 +11,53 @@ _LOGGER = logging.getLogger(__name__)
 
 T = TypeVar("T")
 
+
 class DownoladKeys(Enum):
-    GID = 'gid'
-    STATUS = 'status'
-    TOTAL_LENGTH = 'totalLength'
-    COMPLETED_LENGTH = 'completedLength'
-    UPLOADED_LENGTH = 'uploadLength'
-    BIT_FIELD = 'bitfield'
-    DOWNLOAD_SPEED = 'downloadSpeed'
-    UPLOAD_SPEED = 'uploadSpeed'
-    INFO_HASH = 'infoHash'
-    NUM_SEEDERS = 'numSeeders'
-    SEEDER = 'seeder'
-    PIECE_LENGTH = 'pieceLength'
-    NUM_PIECES = 'numPieces'
-    CONNECTIONS = 'connections'
-    ERROR_CODE = 'errorCode'
-    FOLLOWED_BY = 'followedBy'
-    FOLLOWING = 'following'
-    BELONGS_TO = 'belongsTo'
-    DIR = 'dir'
-    FILES = 'files'
-    BITTORENT = 'bittorrent'
-    VERIFIED_LENGTH = 'verifiedLength'
-    VERIFY_INTEGRITY_PENDING = 'verifyIntegrityPending'
+    GID = "gid"
+    STATUS = "status"
+    TOTAL_LENGTH = "totalLength"
+    COMPLETED_LENGTH = "completedLength"
+    UPLOADED_LENGTH = "uploadLength"
+    BIT_FIELD = "bitfield"
+    DOWNLOAD_SPEED = "downloadSpeed"
+    UPLOAD_SPEED = "uploadSpeed"
+    INFO_HASH = "infoHash"
+    NUM_SEEDERS = "numSeeders"
+    SEEDER = "seeder"
+    PIECE_LENGTH = "pieceLength"
+    NUM_PIECES = "numPieces"
+    CONNECTIONS = "connections"
+    ERROR_CODE = "errorCode"
+    FOLLOWED_BY = "followedBy"
+    FOLLOWING = "following"
+    BELONGS_TO = "belongsTo"
+    DIR = "dir"
+    FILES = "files"
+    BITTORENT = "bittorrent"
+    VERIFIED_LENGTH = "verifiedLength"
+    VERIFY_INTEGRITY_PENDING = "verifyIntegrityPending"
+
 
 class UnauthorizedError(Exception):
     pass
 
-class Command(Generic[T]):
 
+class Command(Generic[T]):
     def __init__(self, method: str, params: list = []):
         self.id = uuid4()
         self.method_name = method
         self.params = params
         self.result_future = None
 
-    def to_json(self, security_token: str = None) -> dict :
+    def to_json(self, security_token: str = None) -> dict:
         params = ["token:" + security_token] if security_token else []
         params.extend(self.params)
 
         return {
-            'jsonrpc': '2.0',
-            'method': self.method_name,
-            'id': str(self.id),
-            'params': params
+            "jsonrpc": "2.0",
+            "method": self.method_name,
+            "id": str(self.id),
+            "params": params,
         }
 
     def build_awaitable_future(self, loop: asyncio.AbstractEventLoop) -> futures.Future:
@@ -70,11 +71,11 @@ class Command(Generic[T]):
         return result
 
     def error_received(self, json_error: dict):
-        if json_error['code'] == 1:
+        if json_error["code"] == 1:
             self._raise_except(UnauthorizedError())
 
     def get_result(self, json_result: dict) -> T:
-        _LOGGER.error('the get_result function should be overriden')
+        _LOGGER.error("the get_result function should be overriden")
 
     def _raise_except(self, exception: Exception):
         if self.result_future and not self.result_future.cancelled():
@@ -82,115 +83,146 @@ class Command(Generic[T]):
         else:
             raise exception
 
-class GetGlobalOption(Command[Options]):
 
+class GetGlobalOption(Command[Options]):
     def __init__(self):
-        super().__init__('aria2.getGlobalOption')
+        super().__init__("aria2.getGlobalOption")
 
     def get_result(self, json_result: dict) -> Options:
         return Options(None, json_result)
 
-class ChangeGlobalOptions(Command[bool]):
 
+class ChangeGlobalOptions(Command[bool]):
     def __init__(self, optionValues: Dict[str, str]):
-        super().__init__('aria2.changeGlobalOption', [optionValues])
+        super().__init__("aria2.changeGlobalOption", [optionValues])
 
     def get_result(self, json_result: dict) -> bool:
-        return json_result == 'OK'
+        return json_result == "OK"
 
     def error_received(self, json_error: dict):
-        return self.result_received('KO')
+        return self.result_received("KO")
+
 
 class GetGlobalStat(Command[Stats]):
-
     def __init__(self):
-        super().__init__('aria2.getGlobalStat')
+        super().__init__("aria2.getGlobalStat")
 
     def get_result(self, json_result: dict) -> Stats:
-        _LOGGER.debug('get_result for GetGlobalStat. ' + str(json_result))
+        _LOGGER.debug("get_result for GetGlobalStat. " + str(json_result))
         return Stats(json_result)
+
 
 class Unpause(Command[str]):
     def __init__(self, gid):
-        super().__init__('aria2.unpause', [gid])
+        super().__init__("aria2.unpause", [gid])
 
     def get_result(self, json_result: Any) -> str:
-        _LOGGER.debug('get_result for unpause. ' + str(json_result))
+        _LOGGER.debug("get_result for unpause. " + str(json_result))
         return json_result
+
 
 class Pause(Command[str]):
     def __init__(self, gid):
-        super().__init__('aria2.pause', [gid])
+        super().__init__("aria2.pause", [gid])
 
     def get_result(self, json_result: dict) -> str:
-        _LOGGER.debug('get_result for pause. ' + str(json_result))
+        _LOGGER.debug("get_result for pause. " + str(json_result))
         return json_result
+
 
 class Remove(Command[str]):
     def __init__(self, gid):
-        super().__init__('aria2.remove', [gid])
+        super().__init__("aria2.remove", [gid])
 
     def get_result(self, json_result: dict) -> str:
-        _LOGGER.debug('get_result for remove. ' + str(json_result))
+        _LOGGER.debug("get_result for remove. " + str(json_result))
         return json_result
+
 
 class AddUri(Command[str]):
     def __init__(self, uris: List[str]):
-        super().__init__('aria2.addUri', [uris])
+        super().__init__("aria2.addUri", [uris])
 
     def get_result(self, json_result: Any) -> str:
-        _LOGGER.debug('get_result for addUri. ' + str(json_result))
+        _LOGGER.debug("get_result for addUri. " + str(json_result))
         return json_result
+
+
+class AddTorrent(Command[str]):
+    def __init__(self, uri: str):
+        super().__init__("aria2.addTorrent", [uri])
+
+    def get_result(self, json_result: Any) -> str:
+        _LOGGER.debug("get_result for addTorrent. " + str(json_result))
+        return json_result
+
 
 class TellActive(Command[List[Download]]):
     def __init__(self, keys: List[DownoladKeys] = []):
-        super().__init__('aria2.tellActive', [[k.value for k in keys]])
+        super().__init__("aria2.tellActive", [[k.value for k in keys]])
 
     def get_result(self, json_result: Any) -> str:
-        _LOGGER.debug('get_result for tellActive. ' + str(json_result))
+        _LOGGER.debug("get_result for tellActive. " + str(json_result))
         return [Download(None, d) for d in json_result]
+
 
 class TellWaiting(Command[List[Download]]):
-    def __init__(self, offset: int = 0, pageSize: int = 1000, keys: List[DownoladKeys] = []):
-        super().__init__('aria2.tellWaiting', [offset, pageSize, [k.value for k in keys]])
+    def __init__(
+        self, offset: int = 0, pageSize: int = 1000, keys: List[DownoladKeys] = []
+    ):
+        super().__init__(
+            "aria2.tellWaiting", [offset, pageSize, [k.value for k in keys]]
+        )
 
     def get_result(self, json_result: Any) -> str:
-        _LOGGER.debug('get_result for tellWaiting. ' + str(json_result))
+        _LOGGER.debug("get_result for tellWaiting. " + str(json_result))
         return [Download(None, d) for d in json_result]
+
 
 class TellStopped(Command[List[Download]]):
-    def __init__(self, offset: int = 0, pageSize: int = 1000, keys: List[DownoladKeys] = []):
-        super().__init__('aria2.tellStopped', [offset, pageSize, [k.value for k in keys]])
+    def __init__(
+        self, offset: int = 0, pageSize: int = 1000, keys: List[DownoladKeys] = []
+    ):
+        super().__init__(
+            "aria2.tellStopped", [offset, pageSize, [k.value for k in keys]]
+        )
 
     def get_result(self, json_result: Any) -> str:
-        _LOGGER.debug('get_result for tellStopped. ' + str(json_result))
+        _LOGGER.debug("get_result for tellStopped. " + str(json_result))
         return [Download(None, d) for d in json_result]
+
 
 class TellStatus(Command[Download]):
     def __init__(self, gid: str, keys: List[DownoladKeys] = []):
-        super().__init__('aria2.tellStatus', [gid, [k.value for k in keys]])
+        super().__init__("aria2.tellStatus", [gid, [k.value for k in keys]])
 
     def get_result(self, json_result: Any) -> str:
-        _LOGGER.debug('get_result for tellStatus. ' + str(json_result))
+        _LOGGER.debug("get_result for tellStatus. " + str(json_result))
         return Download(None, json_result)
+
 
 class MultiCall(Command[List[Any]]):
     def __init__(self, commands: List[Command[Any]]):
-        super().__init__('system.multicall', commands)
+        super().__init__("system.multicall", commands)
 
-    def to_json(self, security_token: str = None) -> dict :
+    def to_json(self, security_token: str = None) -> dict:
         params = []
         for command in self.params:
             command_json = command.to_json(security_token)
-            params.append({'methodName': command_json['method'], 'params': command_json['params']})
+            params.append(
+                {"methodName": command_json["method"], "params": command_json["params"]}
+            )
 
         return {
-            'jsonrpc': '2.0',
-            'method': self.method_name,
-            'id': str(self.id),
-            'params': [params]
+            "jsonrpc": "2.0",
+            "method": self.method_name,
+            "id": str(self.id),
+            "params": [params],
         }
 
     def get_result(self, json_result: Any) -> str:
-        _LOGGER.debug('get_result for multi_call. ' + str(json_result))
-        return [self.params[index].get_result(result[0]) for index, result in enumerate(json_result)]
+        _LOGGER.debug("get_result for multi_call. " + str(json_result))
+        return [
+            self.params[index].get_result(result[0])
+            for index, result in enumerate(json_result)
+        ]
