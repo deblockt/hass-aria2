b7e604fbaadefa9d4133a029f5143d0d23ee3680
Thomas Deblock <deblock.thomas.62@gmail.com>
2025-10-18 18:40:24 +0200

    feat: add waiting/active gid list sensor

--- custom_components/aria2/sensor.py
+++ custom_components/aria2/sensor.py
@@ -4,7 +4,13 @@ import asyncio
 from datetime import timedelta
 import logging
 from custom_components.aria2.aria2_client import WSClient
-from custom_components.aria2.aria2_commands import GetGlobalStat
+from custom_components.aria2.aria2_commands import (
+    DownoladKeys,
+    TellActive,
+    TellStopped,
+    TellWaiting,
+    GetGlobalStat,
+)
 
 from homeassistant.const import CONF_HOST, UnitOfDataRate
 from homeassistant.components.sensor import SensorEntity
@@ -82,11 +88,98 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
                 None,
                 "number of stopped download",
             ),
+            Aria2StateListSensor(
+                ws_client,
+                aria_name,
+                service_attributes,
+                'active',
+                "active gids"
+            ),
+            Aria2StateListSensor(
+                ws_client,
+                aria_name,
+                service_attributes,
+                'waiting',
+                "waiting gids"
+            )
         ],
         True,
     )
 
 
+class Aria2StateListSensor(SensorEntity):
+    """A base class for state gid list sensor."""
+
+    def __init__(
+        self,
+        ws_client,
+        aria_name,
+        service_attributes,
+        state,
+        sensor_name,
+    ):
+        """Initialize the sensor."""
+        self._gid_list = ""
+        self._aria_name = aria_name
+        self._sensor_name = sensor_name
+        self._state = state
+        self._ws_client = ws_client
+        self._service_attributes = service_attributes
+        
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return f"{self._aria_name}-{self._sensor_name}"
+
+    @property
+    def device_info(self):
+        return self._service_attributes
+
+    @property
+    def unique_id(self):
+        """Return the unique id of the entity."""
+        return f"{self._aria_name}-{self._sensor_name}"
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._gid_list
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return None
+
+    @property
+    def should_poll(self):
+        """Return the polling requirement for this sensor."""
+        return False
+
+    @property
+    def available(self):
+        """Could the device be accessed during the last update call."""
+        return True
+
+    async def async_added_to_hass(self):
+        """Handle entity which will be added."""
+        async def on_download_state_updated(download_gid, status):
+            if status == self._state:
+                if not download_gid in self._gid_list:
+                    self._gid_list = self._gid_list + "\n" + download_gid
+            else:
+                self._gid_list = "\n".join(gid for gid in self._gid_list.split("\n") if gid != download_gid)
+
+            self.async_write_ha_state()
+            
+        self._ws_client.on_download_state_updated(on_download_state_updated)
+        
+        state_to_command = {
+            "active": TellActive,
+            "waiting": TellWaiting
+        }
+        downloads = await self._ws_client.call(state_to_command.get(self._state)(keys=[DownoladKeys.GID, DownoladKeys.STATUS]))
+        self._gid_list = "\n".join(d.gid for d in downloads if d.status == self._state)
+        
 class Aria2Sensor(SensorEntity):
     """A base class for all aria2 sensors."""
 
